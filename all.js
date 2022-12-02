cbbabc993f43252e305daaa3eba227907fcf3813
Erwin <afwyousenge@gmail.com>
2022-12-02 16:12:04 +0800

    取得購物車資料，刪除購物車資料為完成

--- all.js
+++ all.js
@@ -13,10 +13,11 @@ const token = 'yCZCYeLTxAb6UAUNbvYpJ2AyYSy1';
 let productList = []
 const productWrap = document.querySelector('.productWrap')
 const productSelect = document.querySelector('.productSelect')
-
+const shoppingCartTable = document.querySelector('.shoppingCart-table')
 // 初始化
 function init() {
   getProductList()
+  getCartList()
 }
 init()
 
@@ -39,7 +40,7 @@ function renderData(arr) {
             <img
               src="${item.images}"
               alt="">
-            <a href="#" class="addCardBtn">加入購物車</a>
+            <a href="#" class="addCardBtn" data-id="${item.id}">加入購物車</a>
             <h3>${item.title}</h3>
             <del class="originPrice">NT$${item.origin_price}</del>
             <p class="nowPrice">NT$${item.price}</p>
@@ -78,13 +79,82 @@ productSelect.addEventListener('change', function (e) {
 //取得購物車
 function getCartList() {
   axios.get(`https://livejs-api.hexschool.io/api/livejs/v1/customer/${api_path}/carts`)
+    .then(function (res) {
+      console.log(res.data.carts)
+      renderCart(res.data.carts)
+    })
 }
 
+//渲染購物車
+function renderCart(arr) {
+  let str = `<tr>
+          <th width="40%">品項</th>
+          <th width="15%">單價</th>
+          <th width="15%">數量</th>
+          <th width="15%">金額</th>
+          <th width="15%"></th>
+        </tr>`
+  arr.forEach(function (item) {
+    str += `
+        <tr>
+          <td>
+            <div class="cardItem-title">
+              <img src="${item.product.images}" alt="">
+              <p>${item.product.title}</p>
+            </div>
+          </td>
+          <td>NT$${item.product.price}</td>
+          <td>${item.quantity}</td>
+          <td>NT$${item.product.price * item.quantity}</td>
+          <td class="discardBtn">
+            <a href="#" class="material-icons" data-id="${item.id}">
+              clear
+            </a>
+          </td>
+        </tr>
+  `
+  })
+  str += `
+            <tr>
+              <td>
+                <a href="#" class="discardAllBtn">刪除所有品項</a>
+              </td>
+              <td></td>
+              <td></td>
+              <td>
+                <p>總金額</p>
+              </td>
+              <td>NT$13,980</td>
+            </tr>`
+  shoppingCartTable.innerHTML = str
+}
 //加入購物車
-
+productWrap.addEventListener('click', function (e) {
+  const target = e.target
+  const id = target.getAttribute('data-id')
+  if (target.classList.value === 'addCardBtn') {
+    // console.log('geteeeeeee')
+    axios.post(`https://livejs-api.hexschool.io/api/livejs/v1/customer/${api_path}/carts`,
+      {
+        "data": {
+          "productId": id,
+          "quantity": 5
+        }
+      })
+    getCartList()
+  }
+})
 
 //刪除購物車
-
+shoppingCartTable.addEventListener('click', function (e) {
+  const target = e.target
+  const id = target.getAttribute('data-id')
+  if (target.classList.value === 'material - icons') {
+    // axios.delete(`https://livejs-api.hexschool.io/api/livejs/v1/customer/${api_path}/carts/${id}`)
+    console.log(135135646351351)
+  }
+}
+)
 
 //送出預定
 
