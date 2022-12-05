2ed6ec4b2f923dc24de3eb2827f27d930c5a109d
afwyou <afwyousenge@gmail.com>
2022-12-05 09:15:03 +0800

    渲染訂單成功，訂單時間、訂單狀態未解決

--- admin.js
+++ admin.js
@@ -25,13 +25,58 @@ const orderPageTable = document.querySelector('.orderPage-table')
 
 
 //取得訂單、渲染
-axios.get(`https://livejs-api.hexschool.io/api/livejs/v1/admin/${api_path}/orders`, {
-  headers: {
-    'Authorization': token,
-  }
-}).then(function (res) {
-  console.log(res)
-})
+function getOrderList() {
+  axios.get(`https://livejs-api.hexschool.io/api/livejs/v1/admin/${api_path}/orders`, {
+    headers: {
+      'Authorization': token,
+    }
+  }).then(function (res) {
+    console.log(res.data.orders)
+    orderList = res.data.orders
+    //組合字串
+    let str = ''
+    str += ` <thead>
+          <tr>
+            <th>訂單編號</th>
+            <th>聯絡人</th>
+            <th>聯絡地址</th>
+            <th>電子郵件</th>
+            <th>訂單品項</th>
+            <th>訂單日期</th>
+            <th>訂單狀態</th>
+            <th>操作</th>
+          </tr>
+        </thead>`
+
+    //動態部分
+    orderList.forEach(item => {
+      str += `<tr>
+          <td>${item.createdAt}</td>
+          <td>
+            <p>${item.user.name}</p>
+            <p>${item.user.tel}</p>
+          </td>
+          <td>${item.user.address}</td>
+          <td>${item.user.email}</td>
+          <td>
+            <p>${item.products.title}</p>
+          </td>
+          <td>2021/03/08</td>
+          <td class="orderStatus">
+            <a href="#">未處理</a>
+          </td>
+          <td>
+            <input type="button" class="delSingleOrder-Btn" value="刪除">
+          </td>
+        </tr>`
+    });
+
+    orderPageTable.innerHTML = str
+
+  })
+}
+getOrderList()
+
 
 //修改訂單
 
